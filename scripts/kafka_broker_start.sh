9c3cb4a23a786782485a07e02637e3654a84e0df
petergdoyle <peter.doyle@travelport.com>
2017-05-11 14:43:08 -0600

    accomodate different versions of kafka for selecting a configuration file

--- scripts/kafka_broker_start.sh
+++ scripts/kafka_broker_start.sh
@@ -16,14 +16,25 @@ if [ -d /tmp/zookeeper ]; then
   fi
 fi
 
-sudo cp -v config/* /usr/kafka/default/config/
 node_name=`hostname |grep -io broker[0-9] |awk '{print tolower($0)}'`
-host_name=`hostname| cut -d"." -f1`
+host_name=`hostname| cut -d"." -f1
+
+kafka_version="10.0.1"
+broker_config_file="not found"
+while [ ! -f "$broker_config_file" ]; then
+  read -e -p "Confirm Kafka Version: " -i "$kafka_version" kafka_version
+  broker_config_file=`find config/ -name "$node_name*$kafka_version*"`
+fi
+
+sudo cp -v config/* /usr/kafka/default/config/
+
 mkdir -p $PWD/logs/$node_name/
 broker_log_file="$PWD/logs/$node_name/kafka_broker_console.log"
-cmd="$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/$node_name.properties > $broker_log_file 2>&1"
+cmd="$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/$broker_config_file > $broker_log_file 2>&1"
 echo "$cmd"
 eval "$cmd" &
 echo "Output will be redirected to $broker_log_file"
 sleep 3
 ps aux |grep java |grep --color -v grep
+sleep 1
+tail -f "$broker_log_file"
