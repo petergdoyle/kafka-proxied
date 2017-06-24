b07c97aeedc9f76f266e72cb41b63cd0ab3c6dfc
petergdoyle <peter.doyle@travelport.com>
2017-06-24 08:07:24 -0400

    fix early exit

--- scripts/install_kafka.sh
+++ scripts/install_kafka.sh
@@ -27,40 +27,36 @@ install_anyway="y"
 if [ -d $kafka_base_location/kafka_$scala_version-$kafka_version ]; then
  read -e -p "It appears kafka is already installed at $kafka_base_location/kafka_$scala_version-$kafka_version. Install again (y/n)? " -i "$install_anyway" install_anyway
 fi
-
-if [ "$install_anyway" == "y" ]; then
-
-  if [ ! -d "$kafka_base_location" ]; then
-   mkdir -p $kafka_base_location
-  fi
-
-  downloadable="kafka_$scala_version-$kafka_version.tgz"
-  download_url="http://www-us.apache.org/dist/kafka/$kafka_version/$downloadable"
-  echo "downloading $download_url..."
-  kafka_home="$kafka_base_location/default"
-
-  curl -O $download_url \
-  && tar -xvf $downloadable -C $kafka_base_location \
-  && rm -f $downloadable \
-  && ln -s $kafka_base_location/kafka_$scala_version-$kafka_version $kafka_home
-
-  if `grep KAFKA_HOME ~/.bash_profile` ; then
-    # replace
-    sed -i "s/export KAFKA_HOME=.*/export KAFKA_HOME=$kafka_home/g" ~/.bash_profile
-  else
-    # insert
-   export KAFKA_HOME=$kafka_base_location/default
-      cat >>~/.bash_profile <<-EOF
-export KAFKA_HOME=$KAFKA_HOME
-EOF
-  fi
-
-   mkdir -p $kafka_base_location/logs \
-   && chmod 1777 $kafka_base_location/logs
-
-   echo -e "\e[7;44;96m*$kafka_version is now installed at $kafka_base_location/default. Log out and log back in to set the proper envariable. \e[0m"
-
+if [ "$install_anyway" != "y" ]; then
+  exit 0
 fi
 
+if [ ! -d "$kafka_base_location" ]; then
+ mkdir -p $kafka_base_location
+fi
+
+downloadable="kafka_$scala_version-$kafka_version.tgz"
+download_url="http://www-us.apache.org/dist/kafka/$kafka_version/$downloadable"
+echo "downloading $download_url..."
+kafka_home="$kafka_base_location/default"
+
+curl -O $download_url \
+&& tar -xvf $downloadable -C $kafka_base_location \
+&& rm -f $downloadable \
+&& ln -s $kafka_base_location/kafka_$scala_version-$kafka_version $kafka_home
+
+if `grep KAFKA_HOME ~/.bash_profile` ; then
+  # replace
+  sed -i "s/export KAFKA_HOME=.*/export KAFKA_HOME=$kafka_home/g" ~/.bash_profile
 else
-   echo -e "\e[7;44;96m*Kafka appears to be installed at $KAFKA_HOME. \e[0m"
+  # insert
+ export KAFKA_HOME=$kafka_base_location/default
+ cat >>~/.bash_profile <<-EOF
+export KAFKA_HOME=$KAFKA_HOME
+EOF
+fi
+
+mkdir -p $kafka_base_location/logs \
+&& chmod 1777 $kafka_base_location/logs
+
+echo -e "\e[7;44;96m*$kafka_version is now installed at $kafka_base_location/default. Log out and log back in to set the proper envariable. \e[0m"
