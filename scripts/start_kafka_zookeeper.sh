abb5c8b42bb7aafcf17141ffbffa14d970dbc2a1
petergdoyle <peter.doyle@travelport.com>
2017-08-07 22:30:25 -0600

    allow for multiple brokers to run

--- scripts/start_kafka_zookeeper.sh
+++ scripts/start_kafka_zookeeper.sh
@@ -9,27 +9,49 @@ if [ ! -z $ZK_PIDS ]; then\
   exit 1
 fi
 
-create_zookeeper_config
-
-cmd="$KAFKA_HOME/bin/zookeeper-server-start.sh $zookeeper_config_file> $zookeeper_runtime_console_log_file 2>&1"
-echo "$cmd"
-prompt=$BOLD$YELLOW"About to start Zookeeper, continue? (y/n): $RESET"
-default_value="y"
-read -e -p "$(echo -e $prompt)" -i $default_value response
-if [ "$response" != 'y' ]; then
-    exit 0
+if [ ! -d $kafka_runtime_config_dir ]; then
+  mkdir -pv $kafka_runtime_config_dir
 fi
 
-eval "$cmd" &
-sleep 2
-PIDS=`ps ax | grep java | grep -i QuorumPeerMain | grep -v grep | awk '{print $1}'`
-if [ ! -z $PIDS ]; then
-  prompt="Tail on log file ($zookeeper_runtime_console_log_file)? (y/n): "
+if [ ! -d $kafka_runtime_console_logs_dir ]; then
+  mkdir -pv $kafka_runtime_console_logs_dir
+fi
+
+if [ ! -f $zookeeper_config_template_file ]; then
+  "cannot continue. no template file named $zookeeper_config_template_file exists"
+  exit 1
+fi
+
+no_instances="1"
+read -e -p "Enter the number of zookeeper instances: " -i "$no_instances" no_instances
+for i in $(eval echo "{1..$no_instances}"); do
+  configure_zookeeper
+  zookeeper_config_file="$kafka_runtime_config_dir/$node_name-zookeeper-$i.properties"
+  cp -vf $zookeeper_config_template_file  $zookeeper_config_file
+  sed -i "s/clientPort=.*/clientPort=$zk_port/g" $zookeeper_config_file
+  zookeeper_runtime_console_log_file="$kafka_base_location/logs/$node_name-zookeeper-$i-console.log"
+
+  cmd="$KAFKA_HOME/bin/zookeeper-server-start.sh $zookeeper_config_file> $zookeeper_runtime_console_log_file 2>&1"
+  echo "$cmd"
+  prompt=$BOLD$YELLOW"About to start Zookeeper instance $i, continue? (y/n): $RESET"
   default_value="y"
-  read -e -p "$prompt" -i $default_value response
-  if [ "$response" == 'y' ]; then
-    tail -f "$zookeeper_runtime_console_log_file"
+  read -e -p "$(echo -e $prompt)" -i $default_value response
+  if [ "$response" != 'y' ]; then
+      exit 0
   fi
-else
-  display_error "zookeeper does not appear to be running"
-fi
+
+  eval "$cmd" &
+  sleep 2
+  PIDS=`ps ax | grep java | grep -i QuorumPeerMain | grep -v grep | awk '{print $1}'`
+  if [[ ! -z $PIDS ]]; then
+    prompt="Tail on log file ($zookeeper_runtime_console_log_file)? (y/n): "
+    default_value="y"
+    read -e -p "$prompt" -i $default_value response
+    if [ "$response" == 'y' ]; then
+      timeout 5s tail "$zookeeper_runtime_console_log_file"
+    fi
+  else
+    display_error "zookeeper does not appear to be running"
+  fi
+
+done
