6993e2acb4a1619bc45f988d7f999cebdcbe6e43
petergdoyle <peter.doyle@travelport.com>
2017-08-07 18:42:33 -0600

    fixed prompt default value

--- scripts/list_topics.sh
+++ scripts/list_topics.sh
@@ -6,8 +6,8 @@ if [ -z $KAFKA_HOME ]; then
   echo "No env var KAFKA_HOME is set. Source your ~/.bash_profile or logout and log back in"
   exit 1
 fi
-
-read -e -p "Enter the zk host/port: " -i "localhost:2181" zk_host_port
+zk_host_port="localhost:2181"
+read -e -p "Enter the zk host/port: " -i "$zk_host_port" zk_host_port
 
 cmd="$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper $zk_host_port"
 echo "$cmd"
@@ -15,5 +15,5 @@ prompt=$BOLD$YELLOW"About to start List Topics as shown, continue? (y/n): $RESET
 default_value="y"
 read -e -p "$(echo -e $prompt)" -i $default_value response
 if [ "$response" == 'y' ]; then
-  eval "$cmd" 
+  eval "$cmd"
 fi
