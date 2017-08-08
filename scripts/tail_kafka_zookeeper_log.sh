e8f1169c5b495aa24233dbc4ed0a5133945661bb
petergdoyle <peter.doyle@travelport.com>
2017-08-08 15:29:59 -0600

    changed to allow for prompting if multiple log files

--- scripts/tail_kafka_zookeeper_log.sh
+++ scripts/tail_kafka_zookeeper_log.sh
@@ -9,4 +9,11 @@ if [ -z $ZK_PIDS ]; then\
   exit 1
 fi
 
+number_of_zookeepers=`find $kafka_runtime_console_logs_dir -type f -name '*broker*' |wc -l`
+selected_zookeper='1'
+if [ $number_of_zookeepers -gt 1 ]; then
+  read -e -p "A total of $number_of_zookeepers broker log file(s) found. Which broker (number)?: " -i "$selected_zookeper" selected_zookeper
+fi
+zookeeper_runtime_console_log_file="$kafka_base_location/logs/$node_name-zookeeper-$selected_zookeper-console.log"
+
 tail -f "$zookeeper_runtime_console_log_file"
