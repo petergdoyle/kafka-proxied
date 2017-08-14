9550232e4bc40a96ff813c38045dbed7434a1ead
petergdoyle <peter.doyle@travelport.com>
2017-08-13 21:23:07 -0600

    display all kafka runtime configs

---
+++ scripts/display_kafka_configs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cd $(dirname $0)
+. ./common.sh
+
+files_found=`find $kafka_runtime_config_dir -type f -name '*properties'|wc -l`
+# selected_broker='1'
+# if [ $files_found -gt 1 ]; then
+#   read -e -p "A total of $files_found broker config file(s) found. Which broker (number)?: " -i "$selected_broker" selected_broker
+# fi
+
+for each in `find $kafka_runtime_config_dir -type f -name '*properties'`; do
+  echo "--- $each ---"
+  grep -v "^#" $each|awk 'NF'
+done
