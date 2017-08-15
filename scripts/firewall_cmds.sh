abf62c91e349226e1b78e7839a3d533637ba500b
petergdoyle <peter.doyle@travelport.com>
2017-08-15 10:40:06 -0600

    latest changes

--- scripts/firewall_cmds.sh
+++ scripts/firewall_cmds.sh
@@ -20,11 +20,11 @@ function firewall_off () {
 
 function add_rich_rules() {
 
-  # firewall-cmd --permanent --new-zone=azure
+  # firewall-cmd --zone=public --permanent --new-zone=azure
   # #40.78.64.141 (HospitalityHertzPocNode0-ip)
-  # firewall-cmd --permanent --zone=azure --add-source=216.113.139.2
+  # firewall-cmd --zone=public --permanent --zone=azure --add-source=216.113.139.2
   # #40.112.255.211 (HospitalityHertzPocNode1-ip)
-  # firewall-cmd --permanent --zone=azure --add-source=40.112.255.211
+  # firewall-cmd --zone=public --permanent --zone=azure --add-source=40.112.255.211
   firewall-cmd --list-all |grep 'rule family'
   local ip="192.168.1.80/90"
   while true; do
@@ -43,25 +43,25 @@ function add_rich_rules() {
 
 }
 
-function remove_rich_rules() {
-
-    firewall-cmd --list-all |grep 'rule family'
-    local ip="192.168.1.80/90"
-    while true; do
-      read -e -p "Enter the inbound IP number: " -i "$ip" ip
-      local port="9091"
-      read -e -p "Enter the inbound port to open (range allowed - 9091-9093): " -i "$port" port
-      cmd="firewall-cmd --zone=public --remove-rich-rule='rule family=ipv4 source address=$ip port port=\"$port\" protocol=tcp accept'"
-      echo "about to run command: $cmd"
-      eval "$cmd"
-      local response="y"
-      read -e -p "Remove another firewall rich-rule? (y/n): " -i "$response" response
-      if [ $response != "y" ]; then
-        break
-      fi
-    done
-
-}
+# function remove_rich_rules() {
+#
+#     firewall-cmd --list-all |grep 'rule family'
+#     local ip="192.168.1.80/90"
+#     while true; do
+#       read -e -p "Enter the inbound IP number: " -i "$ip" ip
+#       local port="9091"
+#       read -e -p "Enter the inbound port to open (range allowed - 9091-9093): " -i "$port" port
+#       cmd="firewall-cmd --zone=public --remove-rich-rule='rule family=ipv4 source address=$ip port port=\"$port\" protocol=tcp accept'"
+#       echo "about to run command: $cmd"
+#       eval "$cmd"
+#       local response="y"
+#       read -e -p "Remove another firewall rich-rule? (y/n): " -i "$response" response
+#       if [ $response != "y" ]; then
+#         break
+#       fi
+#     done
+#
+# }
 
 function list_rich_rules() {
   OLD_IFS=$IFS
@@ -72,18 +72,44 @@ function list_rich_rules() {
   IFS=$OLD_IFS
 }
 
+function remove_rich_rules() {
+  list_rich_rules| sed s/--add-rich-rule/--remove-rich-rule/g
+}
+
+
+# all
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.81" port port="9091-9093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.82" port port="9091-9093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.83" port port="9091-9093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="40.112.255.211" port port="9091-9093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="40.78.64.141" port port="9091-9093" protocol="tcp" accept'
+
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.81" port port="19091-19093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.82" port port="19091-19093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.83" port port="19091-19093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="40.112.255.211" port port="19091-19093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="40.78.64.141" port port="19091-19093" protocol="tcp" accept'
+
+firewall-cmd --zone=public --add-masquerade --permanent
 
 # engine1
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="40.112.255.211" port port="9091-9093" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="40.78.64.141" port port="9091-9093" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.82" port port="9091-9093" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.83" port port="9091-9093" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="40.112.255.211" port port="2181" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="40.78.64.141" port port="2181" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.81" port port="9091-9093" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.81" port port="2181" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.82" port port="2181" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.83" port port="2181" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.141" port port="2181" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.141" port port="9091-9093" protocol="tcp" accept'
-# firewall-cmd --add-rich-rule='rule family="ipv4" source address="192.168.1.81" port port="19091-19093" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-forward-port=port=12181:proto=tcp:toport=2181
+firewall-cmd --zone=public --permanent --add-forward-port=port=19091:proto=tcp:toport=9091
+firewall-cmd --zone=public --permanent --add-rich-rule 'rule family="ipv4" source address=192.168.0.14 accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.81" port port="2181" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.82" port port="2181" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.83" port port="2181" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="40.78.64.141" port port="2181" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="40.112.255.211" port port="2181" protocol="tcp" accept'
+
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.81" port port="12181" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.82" port port="12181" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="192.168.1.83" port port="12181" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="40.78.64.141" port port="12181" protocol="tcp" accept'
+firewall-cmd --zone=public --permanent --add-rich-rule='rule family="ipv4" source address="40.112.255.211" port port="12181" protocol="tcp" accept'
+
+# engine2
+firewall-cmd --zone=public --add-forward-port=port=19092:proto=tcp:toport=9092
+
+# engine3
+firewall-cmd --zone=public --add-forward-port=port=19093:proto=tcp:toport=9093
--- scripts/tail_kafka_zookeeper_log.sh
+++ scripts/tail_kafka_zookeeper_log.sh
@@ -14,6 +14,7 @@ selected_zookeper='1'
 if [ $number_of_zookeepers -gt 1 ]; then
   read -e -p "A total of $number_of_zookeepers broker log file(s) found. Which broker (number)?: " -i "$selected_zookeper" selected_zookeper
 fi
-zookeeper_runtime_console_log_file="$kafka_base_location/logs/$node_name-zookeeper-$selected_zookeper-console.log"
+
+zookeeper_runtime_console_log_file="$kafka_base_location/logs/$node_name-zookeeper-console-$i.log"
 
 tail -f "$zookeeper_runtime_console_log_file"
