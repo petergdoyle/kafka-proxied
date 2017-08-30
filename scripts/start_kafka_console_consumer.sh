8ea5e0872bbffff3fa1737ed5395b0cb2828cad4
petergdoyle <peter.doyle@travelport.com>
2017-08-30 07:01:38 -0600

    fixed issue modifying truststore location

--- scripts/start_kafka_console_consumer.sh
+++ scripts/start_kafka_console_consumer.sh
@@ -53,7 +53,7 @@ if [ "$response" == "y" ]; then
 
   truststore_password="majiic"
   read -e -p "Specify the location of the truststore password: " -i "$truststore_password" truststore_password
-  sed -i "s?ssl.truststore.location=#REPLACE#?ssl.truststore.password=$truststore_location?g" $consumer_ssl_config_file
+  sed -i "s?ssl.truststore.location=#REPLACE#?ssl.truststore.location=$truststore_location?g" $consumer_ssl_config_file
   sed -i "s/ssl.truststore.password=#REPLACE#/ssl.truststore.password=$truststore_password/g" $consumer_ssl_config_file
 
   consumer_ssl_config="--consumer.config $consumer_ssl_config_file"
