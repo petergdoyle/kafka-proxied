0f175a5e28831c63e51821450a30fe39e245d39c
petergdoyle <peter.doyle@travelport.com>
2017-09-06 12:43:51 -0600

    added script to run dockerized mirror-maker single container

---
+++ scripts/docker_run_mirror_maker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cmd="bin/kafka-mirror-maker.sh \
+--consumer.config /vagrant/local/kafka/config/kafka-proxied-mm_consumer.properties \
+--producer.config /vagrant/local/kafka/config/kafka-proxied-mm_producer.properties \
+--whitelist=\"kafka-simple-topic-1\""
