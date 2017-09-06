5de0bdfb6603c2cf9cd19cfdbcbc06d39d472132
petergdoyle <peter.doyle@travelport.com>
2017-09-06 15:02:08 -0600

    split out installing java, needs run as root

--- scripts/install_kafka.sh
+++ scripts/install_kafka.sh
@@ -18,37 +18,8 @@ EOF
 
 java -version > /dev/null 2>&1
 if [ $? -eq 127 ]; then
-
-  prompt="It appears java-jdk-8 is not installed. Do you want to install it (y/n)? "
-  default_value="y"
-  read -e -p "$(echo -e $BOLD$YELLOW$prompt $cmd $RESET)" -i "$default_value" install_java
-  if [ "$install_java" != 'y' ]; then
-    exit 0
-  fi
-
-  mkdir -pv /usr/java
-  yum install -y java-1.8.0-openjdk*
-  java_home=`alternatives --list |grep jre_1.8.0_openjdk| awk '{print $3}'`
-  ln -s "$java_home" /usr/java/default
-  export JAVA_HOME=/usr/java/default
-  cat >/etc/profile.d/java.sh <<-EOF
-export JAVA_HOME=$JAVA_HOME
-EOF
-
-  export JAVA_HOME='/usr/java/default'
-  cat >/etc/profile.d/java.sh <<-EOF
-export JAVA_HOME=$JAVA_HOME
-EOF
-
-  # register all the java tools and executables to the OS as executables
-  install_dir="$JAVA_HOME/bin"
-  for each in $(find $install_dir -executable -type f) ; do
-    name=$(basename $each)
-    alternatives --install "/usr/bin/$name" "$name" "$each" 99999
-  done
-
-else
-  echo -e "\e[7;44;96m*java-jdk-8 already appears to be installed. skipping.\e[0m"
+  display_error "Jdk 8 is not installed. Run install_jdk8sh as root."
+  exit 1
 fi
 
 
@@ -68,7 +39,7 @@ if [ -d $kafka_installation_dir ]; then
     prompt="It appears KAFKA_HOME was not set. It is now set. You will need to source your ~/.bash_profile to continue running scripts."
     echo -e $BOLD$YELLOW$prompt$RESET
   fi
-  
+
   install_anyway="n"
   prompt="It appears kafka is already installed at $kafka_installation_dir, Install it again (y/n)? "
   default_value="$install_anyway"
