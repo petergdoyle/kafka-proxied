5a3155c5a65fe262f3eb42defd251183f2e3619d
petergdoyle <peter.doyle@travelport.com>
2017-09-06 17:55:31 -0600

    find out ip number from hostname lookup

--- docker/docker_run_mirror_maker.sh
+++ docker/docker_run_mirror_maker.sh
@@ -10,9 +10,11 @@ cmd="bin/kafka-mirror-maker.sh \
 cmd_shell="/bin/bash"
 
 host_consumer=`cat kafka/config/mm_consumer.properties |sed -n 's/^bootstrap.servers=//p' |sed 's/:.*$//'`
-read -e -p "Enter ip number for consumer $host_consumer: " -i "192.168.1.91" host_consumer_ip
+host_consumer_ip=`ping -c 1 $host_consumer| head -n1| sed 's/.*(\(.*\))/\1/' |sed 's/:.*//'`
+read -e -p "Enter ip number for consumer $host_consumer: " -i "$host_consumer_ip" host_consumer_ip
 host_producer=`cat kafka/config/mm_producer.properties |sed -n 's/^bootstrap.servers=//p' |sed 's/:.*$//'`
-read -e -p "Enter ip number for producer $host_producer: " -i "192.168.1.92" host_producer_ip
+host_producer_ip=`ping -c 1 $host_producer| head -n1| sed 's/.*(\(.*\))/\1/' |sed 's/:.*//'`
+read -e -p "Enter ip number for producer $host_producer: " -i "$host_producer_ip" host_producer_ip
 
 docker_cmd="docker run -ti --rm --add-host=$host_consumer:$host_consumer_ip --add-host=$host_producer:$host_producer_ip mycompany/kafka $cmd"
 
