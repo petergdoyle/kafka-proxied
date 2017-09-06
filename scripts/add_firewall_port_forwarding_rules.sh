319c31c9eb51d0323ffe442c169a67b4c0f60dbf
petergdoyle <peter.doyle@travelport.com>
2017-09-06 10:18:04 -0600

    work to date

---
+++ scripts/add_firewall_port_forwarding_rules.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+. ./common.sh
+
+if [[ $EUID -ne 0 ]]; then
+  display_error "This script must be run as root"
+  exit 1
+fi
+
+prompt=$BOLD$YELLOW"About to modify firewall rules. Continue (y/n): $RESET"
+default_value="y"
+read -e -p "$(echo -e $prompt)" -i $default_value response
+if [ "$response" != 'y' ]; then
+  exit 0
+fi
+
+permanent=""
+
+firewall-cmd --zone=public $permanent --add-masquerade
+
+no_instances="1"
+read -e -p "Enter the number of broker instances: " -i "$no_instances" no_instances
+
+firewall-cmd --zone=public $permanent --add-forward-port=port=9091:proto=tcp:toaddr=192.168.1.81
--- scripts/common.sh
+++ scripts/common.sh
@@ -58,7 +58,8 @@ fi
 parent_dir="$(dirname "$(pwd)")"
 
 # kafka_version="0.11.0.0"
-kafka_version="0.10.1.1"
+# kafka_version="0.10.1.1"
+kafka_version="0.9.0.1"
 scala_version="2.11"
 
 if [[ $EUID -eq 0 ]]; then #check if run as root to determine where to install kafka
