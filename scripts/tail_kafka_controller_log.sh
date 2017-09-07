4fed730effb8368c7fce649dc3e444c7e2766bac
Peter Doyle <peter@engine2.cleverfishsoftware.com>
2017-09-07 14:03:21 -0600

    changed to run for 5s

--- scripts/tail_kafka_controller_log.sh
+++ scripts/tail_kafka_controller_log.sh
@@ -6,6 +6,6 @@ if [ ! -f $kafka_controller_log_file ]; then
   display_error "the file $kafka_controller_log_file does not exist."
   exit 1
 fi
-cmd="tail -f $kafka_controller_log_file"
+cmd="timeout 5s tail -f $kafka_controller_log_file"
 display_command "$cmd"
 eval "$cmd"
