94c48a147990d2f08db6884da059e8fb4a4bad31
petergdoyle <peter.doyle@travelport.com>
2017-11-27 21:06:31 -0700

    fixed installation for maven 3

--- install/install_maven.sh
+++ install/install_maven.sh
@@ -1,49 +1,32 @@
 #!/bin/sh
+. ../kafka/common.sh
 
 maven_version='3.3.9'
-
 eval 'mvn -version' > /dev/null 2>&1
 if [ $? -eq 127 ]; then
 
-  if [[ $EUID -eq 0 ]]; then
-    maven_base='/usr/maven'
-  else
-    path='../../local/maven'
-    mkdir -pv $path
-    maven_base=$(readlink -f $path)
-  fi
-  maven_home="$maven_base/default"
+  maven_home="$local_maven_dir/default"
 
   echo "downloading apache-maven-$maven_version..."
-  #install maven
-  curl -O http://www-us.apache.org/dist/maven/maven-3/$maven_version/binaries/apache-maven-$maven_version-bin.tar.gz \
-    && tar -xvf apache-maven-$maven_version-bin.tar.gz -C $maven_base \
-    && ln -s $maven_base/apache-maven-$maven_version $maven_home \
+  download_url=http://www-us.apache.org/dist/maven/maven-3/$maven_version/binaries/apache-maven-$maven_version-bin.tar.gz
+  if [ ! -d $local_maven_dir ]; then
+    mkdir -pv $local_maven_dir
+  fi
+
+  curl -O $download_url \
+    && tar -xvf apache-maven-$maven_version-bin.tar.gz -C $local_maven_dir \
+    && ln -s $local_maven_dir/apache-maven-$maven_version $maven_home \
     && rm -f apache-maven-$maven_version-bin.tar.gz
 
   export MAVEN_HOME=$maven_home
 
-  if [[ $EUID -eq 0 ]]; then
-
-  alternatives --display java |grep -e '^/usr/maven/default/bin/mvn' > /dev/null 2>&1
-  if [ $? -eq 1 ]; then
-    alternatives --install "/usr/bin/mvn" "mvn" "$maven_home/bin/mvn" 99999
-    cat >/etc/profile.d/maven.sh <<-EOF
+  if ! grep -q MAVEN_HOME ~/.bash_profile; then
+  cat >>~/.bash_profile <<-EOF
 export MAVEN_HOME=$MAVEN_HOME
+export PATH=\$PATH:\$MAVEN_HOME/bin
 EOF
-  fi
-
-  else
-
-    if ! grep -q MAVEN_HOME ~/.bash_profile; then
-    cat >>~/.bash_profile <<-EOF
-export MAVEN_HOME=$MAVEN_HOME
-alias mvn=$MAVEN_HOME/bin/mvn
-EOF
-  fi
-
   fi
 
 else
-  echo -e "\e[7;44;96mapache-maven-$maven_version already appears to be installed. skipping."
+  display_info "apache-maven-$maven_version already appears to be installed. skipping."
 fi
