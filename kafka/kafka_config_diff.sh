89c9912e025b3923838360a43614eb9e43c0ff89
petergdoyle <peter.doyle@travelport.com>
2017-11-29 09:54:24 -0700

    adding support to manage kafka versions

---
+++ kafka/kafka_config_diff.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+. ../common.sh
+
+v1='0.10.1.1'
+v2='0.10.2.1'
+for each in `find config/$v1 -type f| sed "s/.*\///"`; do
+  if [ -f config/$v2/$each ]; then
+    echo -e "\n\n*** compare config/$v1/$each -> config/$v2/$each ***\n"
+    diff -qy --suppress-common-lines config/$v1/$each config/$v2/$each
+  else
+    display_warn "config/$v2/$each does not exist"
+  fi
+done
