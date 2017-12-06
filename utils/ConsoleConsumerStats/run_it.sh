6a49fa63272a5dd9975e69efde8339b2b1e0bb6e
petergdoyle <peter.doyle@travelport.com>
2017-12-06 08:37:31 -0700

    consumer working

--- utils/ConsoleConsumerStats/run_it.sh
+++ utils/ConsoleConsumerStats/run_it.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-        # String bootstrapServers = args[0];
-        # String consumerGroup = args[1];
-        # String consumerId = args[2];
-        # List<String> topics = Arrays.asList(args[3].split(","));
-        # long sleep = Long.parseLong(args[4]);
+BOLD="\033[1m"
+YELLOW="\033[38;5;11m"
+GREEN="\033[1,32m"
+BLUE="\033[1;36m"
+VIOLET="\033[1;34m"
+RESET="\033[0m"
+
+# String bootstrapServers = args[0];
+# String consumerGroup = args[1];
+# String consumerId = args[2];
+# List<String> topics = Arrays.asList(args[3].split(","));
+# long sleep = Long.parseLong(args[4]);
 
 bootstrap_server="localhost:9091"
 read -e -p "Enter Kafka bootstrap server for kafka conxumer to take data: " -i "$bootstrap_server" bootstrap_server
@@ -25,4 +32,15 @@ read -e -p "Enter the topic name: " -i "$topic" topic
 sleep="0"
 read -e -p "Enter the sleep time: " -i "$sleep" sleep
 
-cmd="java -jar target/ConsoleConsumerStats-1.0-SNAPSHOT.jar $bootstrap_server $consumer_group_id $consumer_id $sleep"
+cmd="java -jar target/ConsoleConsumerStats-1.0-SNAPSHOT.jar $bootstrap_server $consumer_group_id $consumer_id $topic $sleep"
+display_command "$cmd"
+echo -e $BOLD$VIOLET"[info] $cmd"$RESET
+
+prompt=$BOLD$YELLOW"About to run command, continue? (y/n): $RESET"
+default_value="y"
+read -e -p "$(echo -e $prompt)" -i $default_value response
+if [ "$response" != 'y' ]; then
+  exit 0
+fi
+
+eval "$cmd"
