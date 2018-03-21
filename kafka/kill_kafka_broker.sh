2545ad5893aa6a136e97897f7305faf28a416251
J. Bromley <jay@ai.co>
2018-03-21 14:52:22 -0700

    Fixed error in kill_kafka_mirror_maker.sh.
    There was some extra text after the kill command that was causing
    an error message. Fixed it. Also changed kill scripts to send
    signal TERM rather than KILL to give the processes a chance to
    do cleanup. Finally, added some feedback on the status of the
    kill command.

--- kafka/kill_kafka_broker.sh
+++ kafka/kill_kafka_broker.sh
@@ -5,13 +5,16 @@ cd $(dirname $0)
 PIDS=$(ps ax | grep -i 'kafka\.Kafka' | grep java | grep -v grep | awk '{print $1}')
 
 if [ -z "$PIDS" ]; then
-  display_error "No kafka broker process(es) found to stop"
+  display_error "No kafka broker process(es) found to stop."
   exit 0
 else
   for each in $PIDS; do
-    msg="about to kill process(es): $each"
-    display_warn $msg
+    display_warn "about to terminate process ${each}..."
     sleep 1
-    kill -9 $each
+    if kill -TERM $each ; then
+	display_info "Terminated process ${each}."
+    else
+	display_error "Failed to terminate process ${each}!"
+    fi
   done
 fi
--- kafka/kill_kafka_mirror_maker.sh
+++ kafka/kill_kafka_mirror_maker.sh
@@ -5,13 +5,16 @@ cd $(dirname $0)
 PIDS=$(ps ax | grep java | grep -i MirrorMaker | grep -v grep | awk '{print $1}')
 
 if [ -z "$PIDS" ]; then
-  display_error "No kafka mirror-maker process(es) found to stop"
+  display_error "No Kafka Mirror Maker process(es) found to stop."
   exit 0
 else
   for each in $PIDS; do
-    msg="about to kill process(es): $each"
-    display_warn $msg
+    display_warn "about to terminate process ${each}..."
     sleep 1
-    kill -9 TERM $each
+    if kill -TERM $each ; then
+        display_info "Terminated process ${each}."
+    else
+        display_error "Failed to terminate process ${each}!"
+    fi
   done
 fi
